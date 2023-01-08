ae64f5f9668bdbae9f5958781d400d4e07e583e4
= <jujatin@gmail.com>
2023-01-08 17:59:29 +0530

    import babel

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import './css/Css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "babel-polyfill"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
